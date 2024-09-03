bb758c94221f9886b40cf5925dadce5441761f7e
Tere <teresaolivaresrueda@gmail.com>
2024-09-03 14:03:07 -0400

    conVite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <NavBar />
+
+  <router-view />
+  <MediosPago />
+  <FooterComponents />
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import MediosPago from '@/components/MediosPago.vue'
+import FooterComponents from '@/components/FooterComponents.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+    FooterComponents,
+    MediosPago
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <CardComponents />
+</template>
+
+<script>
+import CardComponents from '@/components/CardComponents.vue'
+
+// @ is an alias to /src
+
+export default {
+  components: {
+    CardComponents
+  }
+}
+</script>
